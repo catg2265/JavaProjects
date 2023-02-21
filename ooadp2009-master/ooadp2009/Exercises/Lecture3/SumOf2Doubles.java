4b182fc25d0a18af1cd40268257d8e7b04e419f2
catg2265 <cgrave20@student.aau.dk>
2023-02-21 12:21:31 +0100

    End of lecture 3

---
+++ ooadp2009-master/ooadp2009/Exercises/Lecture3/SumOf2Doubles.java
@@ -0,0 +1,22 @@
+package Lecture3;
+
+import java.util.Scanner;
+
+public class SumOf2Doubles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("The purpose of the program is to take the sum of two double values");
+		
+		System.out.print("Write the first double here: ");
+		double a = sc.nextDouble();
+		System.out.print("Write the second double here: ");		
+		double b = sc.nextDouble();
+		System.out.println("The sum of the doubles are " + (a+b));
+		sc.close();
+	}
+
+}
